1c30386fcea38ed96c60585e842220726b0654ae
Leonard Breitkopf <feanaro100@hotmail.com>
2014-02-07 14:55:34 +0100

    Overview Done - Included the new filters - Changed price position

--- Views/Shop/Overview.php
+++ Views/Shop/Overview.php
@@ -4,118 +4,103 @@
 
 <div class="overview-wrap">
     <div class="overview-category-view">
+        <div class="search-input-wrap">
+            <label for="search">Product Naam</label>
+            <input type="text" id="search" placeholder="Product naam" />
+        </div>
+        <a href="#" class="overview-search-button login-button"><i class="fa fa-search"></i>&nbsp;Zoeken</a>
     </div>
     <div class="overview-products">
         <ul class="overview-products-list">
             <li class="overview-product">
                 <img src="http://placehold.it/60x50" alt="Product Foto" />
                 <h1>Awesome artikel 1</h1>
-                <div>
-                    <p>&euro;833,95</p>
-                    <a href="Product/1">
+                <p>&euro; 833,95</p>
+                <a href="Product/1">
                     Meer info
-                        <i class="fa fa-chevron-circle-right"></i>
-                    </a>
-                </div>
+                    <i class="fa fa-chevron-circle-right"></i>
+                </a>
             </li>
             <li class="overview-product">
                 <img src="http://placehold.it/60x50" alt="Product Foto" />
                 <h1>Awesome artikel 2</h1>
-                <div>
-                    <p>&euro;255,95</p>
-                    <a href="Product/2">
+                <p>&euro; 255,95</p>
+                <a href="Product/2">
                     Meer info
-                        <i class="fa fa-chevron-circle-right"></i>
-                    </a>
-                </div>
+                    <i class="fa fa-chevron-circle-right"></i>
+                </a>
             </li>
             <li class="overview-product">
                 <img src="http://placehold.it/60x50" alt="Product Foto" />
                 <h1>Awesome artikel 3</h1>
-                <div>
-                    <p>&euro;422,95</p>
-                    <a href="Product/3">
+                <p>&euro; 422,95</p>
+                <a href="Product/3">
                     Meer info
-                        <i class="fa fa-chevron-circle-right"></i>
-                    </a>
-                </div>
+                    <i class="fa fa-chevron-circle-right"></i>
+                </a>
             </li>
             <li class="overview-product">
                 <img src="http://placehold.it/60x50" alt="Product Foto" />
                 <h1>Awesome artikel 4</h1>
-                <div>
-                    <p>&euro;364,95</p>
-                    <a href="Product/4">
+                <p>&euro; 364,95</p>
+                <a href="Product/4">
                     Meer info
-                        <i class="fa fa-chevron-circle-right"></i>
-                    </a>
-                </div>
+                    <i class="fa fa-chevron-circle-right"></i>
+                </a>
             </li>
             <li class="overview-product">
                 <img src="http://placehold.it/60x50" alt="Product Foto" />
                 <h1>Awesome artikel 5</h1>
-                <div>
-                    <p>&euro;84,95</p>
-                    <a href="Product/5">
+                <p>&euro; 84,95</p>
+                <a href="Product/5">
                     Meer info
-                        <i class="fa fa-chevron-circle-right"></i>
-                    </a>
-                </div>
+                    <i class="fa fa-chevron-circle-right"></i>
+                </a>
             </li>
             <li class="overview-product">
                 <img src="http://placehold.it/60x50" alt="Product Foto" />
                 <h1>Awesome artikel 6</h1>
-                <div>
-                    <p>&euro;174,95</p>
-                    <a href="Product/6">
+                <p>&euro; 174,95</p>
+                <a href="Product/6">
                     Meer info
-                        <i class="fa fa-chevron-circle-right"></i>
-                    </a>
-                </div>
+                    <i class="fa fa-chevron-circle-right"></i>
+                </a>
             </li>
             <li class="overview-product">
                 <img src="http://placehold.it/60x50" alt="Product Foto" />
                 <h1>Awesome artikel 7</h1>
-                <div>
-                    <p>&euro;964,95</p>
-                    <a href="Product/7">
+                <p>&euro; 964,95</p>
+                <a href="Product/7">
                     Meer info
-                        <i class="fa fa-chevron-circle-right"></i>
-                    </a>
-                </div>
+                    <i class="fa fa-chevron-circle-right"></i>
+                </a>
             </li>
             <li class="overview-product">
                 <img src="http://placehold.it/60x50" alt="Product Foto" />
                 <h1>Awesome artikel 8</h1>
-                <div>
-                    <p>&euro;149,95</p>
-                    <a href="Product/8">
+                <p>&euro; 149,95</p>
+                <a href="Product/8">
                     Meer info
-                        <i class="fa fa-chevron-circle-right"></i>
-                    </a>
-                </div>
+                    <i class="fa fa-chevron-circle-right"></i>
+                </a>
             </li>
             <li class="overview-product">
                 <img src="http://placehold.it/60x50" alt="Product Foto" />
                 <h1>Awesome artikel 9</h1>
-                <div>
-                    <p>&euro;299,95</p>
-                    <a href="Product/9">
+                <p>&euro; 299,95</p>
+                <a href="Product/9">
                     Meer info
-                        <i class="fa fa-chevron-circle-right"></i>
-                    </a>
-                </div>
+                    <i class="fa fa-chevron-circle-right"></i>
+                </a>
             </li>
             <li class="overview-product">
                 <img src="http://placehold.it/60x50" alt="Product Foto" />
                 <h1>Awesome artikel 10</h1>
-                <div>
-                    <p>&euro;99,95</p>
-                    <a href="Product/10">
+                <p>&euro; 99,95</p>
+                <a href="Product/10">
                     Meer info
-                        <i class="fa fa-chevron-circle-right"></i>
-                    </a>
-                </div>
+                    <i class="fa fa-chevron-circle-right"></i>
+                </a>
             </li>
         </ul>
     </div>
